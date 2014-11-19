62e3eb2b014993061fd3dccbff6b2692426fd57d
rooooooooob <raftias@gmail.com>
2014-11-18 22:41:25 -0800

    connects to ircnetwork, responds to pings, and joins channels

---
+++ src/main.erl
@@ -0,0 +1,53 @@
+-module(main).
+-export([connect/2, main_loop/1]).
+
+connect(Address, Port) ->
+	%hardcoded for now, change later
+	UserName = "BeardBot",
+	UserMode = 0,%no modes
+	UserDescription = "I am an erlang bot",
+	{ok, Socket} = gen_tcp:connect(Address, Port, [{packet, line}]),
+	gen_tcp:send(Socket, "NICK " ++ UserName ++ "\r\n"),
+	%is of the form: USER username usermode * :Description
+	UserInfo = "USER " ++ UserName ++ " " ++ integer_to_list(UserMode) ++ " * :" ++ UserDescription ++ "\r\n",
+	gen_tcp:send(Socket, UserInfo),
+	main_loop(Socket).
+
+handle_message(Socket, ["PING"], Rest) ->
+	gen_tcp:send(Socket, "PONG " ++ Rest ++ "\r\n");
+
+handle_message(Socket, _, _) ->
+	ignored.
+
+main_loop(Socket) ->
+	receive
+		{tcp, Socket, Data} ->
+			io:format("Received:~n~s~n~n", [Data]),
+			ByColon = string:tokens(Data, ":"),
+			BySpace = string:tokens(hd(ByColon), " "),
+			LaterArgs = if 
+				length(ByColon) > 1 ->
+					lists:nth(2, ByColon);
+				true ->
+					[]
+			end,
+			handle_message(Socket, BySpace, LaterArgs),
+			main_loop(Socket);
+		{error, timeout} ->
+			io:format("TIMEOUT received~n"),
+			gen_tcp:close(Socket);
+		{error, OtherReason} ->
+			io:format("ERROR occured: ~s", [OtherReason]),
+			gen_tcp:close(Socket);
+		{join, Channel} ->
+			gen_tcp:send(Socket, "JOIN :" ++ Channel ++ "\r\n"),
+			main_loop(Socket);
+		{say, Channel, Message} ->
+			gen_tcp:send(Socket, "PRIVMSG " ++ Channel ++ " :" ++ Message ++ "\r\n"),
+			main_loop(Socket);
+		quit ->
+			gen_tcp:send("QUIT :Client closed"),
+			gen_tcp:close(Socket),
+			io:format("QUIT received~n", []),
+			exit(stopped)
+	end.
